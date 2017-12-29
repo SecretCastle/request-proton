3a04a709b3e69dc76c54c23a1c9466959cd26fb2
SecretCastle <chenfangzhong@hotmail.com>
2017-12-29 17:55:02 +0800

    switch to plupload

--- src/request-upload.js
+++ src/request-upload.js
@@ -1,22 +1,5 @@
-const OSS = require('ali-oss');
+const plupload = require('plupload');
 
-// OSS REGION LIST https://help.aliyun.com/document_detail/31837.html?spm=5176.doc32070.2.9.15JU6d
-
-// 定义基础信息
-const REGION = 'oss-cn-hangzhou';
-const ACCESSKEYID = '';
-const ACCESSKETSCRET = '';
-const BUCKET = '';  // bucket name
-
-// 创建OSS实例
-const client = new OSS({
-  region: REGION,
-  accessKeyId: ACCESSKEYID,
-  accessKeySecret : ACCESSKETSCRET,
-  bucket: BUCKET,
-});
-
-
-const upload_file = async () => {
-  
-}
+const ACCESSKET = '';
+const ACCESSID = '';
+const HOST = '';
