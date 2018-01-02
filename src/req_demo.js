9cf20ef32520b94b00df8387764fec72644b573f
SecretCastle <henrychen9314@gmail.com>
2018-01-02 23:45:12 +0800

    plupload

---
+++ src/req_demo.js
@@ -0,0 +1,40 @@
+import { req, multiRequestWithPromise, multiRequest } from './core/request.js';
+
+// 单个请求
+req({ url: '/topics', method: 'get', param: {}, type: 'xxx' }).then(res => {
+  console.log(res);
+});
+
+// 多请求 1
+multiRequestWithPromise([{
+    url: '/topics'
+  },
+  {
+    url: '/topics'
+  }
+]).then(res => {
+  console.log('多请求1', res);
+})
+
+
+// 多请求2
+multiRequest([{
+    url: '/topics'
+  },
+  {
+    url: '/topics'
+  }
+]).then(res => {
+  console.log('多请求2', res);
+})
+
+
+// example
+const fn1 = async() => {
+  const res1 = await req({ url: '/topics', method: 'get', param: {}, type: 'xxx' });
+  console.log('请求实例1', res1);
+  const res2 = await multiRequestWithPromise([{ url: '/topics' }, { url: '/topics' }])
+  console.log('请求实例2', res2);
+}
+
+fn1()
---
+++ src/upload_demo.js
@@ -0,0 +1,17 @@
+const plupload = require('plupload');
+
+const ACCESSID = '';
+const ACCESSKEY = '';
+const HOST = '';
+const DIR_UPLOAD = '/';
+const NOW = Date.parse(new Date()) / 1000;
+
+let upload_name = '';
+
+const uploader = new plupload.Uploader({
+
+})
+
+export default function upload_proton(btnid) {
+
+}
