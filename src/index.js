1a4a8c9f0e59b9023f0868d179944d3a55d80bda
SecretCastle <chenfangzhong@hotmail.com>
2018-01-03 16:59:48 +0800

    oss 直传 react版本

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Uploader from './core/request-upload';
-
-const uploaded = (filepath) => {
-  console.log(filepath);
-}
-
-ReactDOM.render(
-  <Uploader type={"image"} id={"upload_product"} success={uploaded}/>
-  ,
-  document.getElementById('app')
-)
+import './upload_demo';
--- src/upload_demo.js
+++ src/upload_demo.js
@@ -1,1 +1,13 @@
-console.log('building');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Uploader from './core/request-upload';
+
+const uploaded = (filepath) => {
+  console.log(filepath);
+}
+
+ReactDOM.render(
+  <Uploader type={"image"} id={"upload_product"} success={uploaded}/>
+  ,
+  document.getElementById('app')
+)
