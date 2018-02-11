74390afea5a3d242e8aabcf8d05cacc0bef2ca53
SecretCastle <chenfangzhong@hotmail.com>
2018-02-11 17:57:26 +0800

    problems

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import 'babel-polyfill';
 
 
+/**
+ * 
+ * problems:
+ * 1、request-upload-baidu 再safari下，无法上传accept指定的文件类型
+ */
+
 // react test
-// import './upload_demo';
+import './upload_demo';
 
 // request test
-import './req_demo';
+// import './req_demo';
 
 // PureComponent Test
 // import './extra/purecomponents';
